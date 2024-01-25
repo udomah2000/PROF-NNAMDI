e67f5b931c988a974a1fe5207aab61e69439ee02
Pius Udoma <udomahpius5@gmail.com>
2024-01-25 16:24:10 +0100

    init

---
+++ layout/scripts/books.js
@@ -0,0 +1,261 @@
+const book = [
+    {
+        id: 1,
+        title: `Mutsvairo, B. and Ekeanyanwu, N. T. (Eds., 2022)`,
+        body: ` Media and Communication in
+        Nigeria: Conceptual Connections, Crossroads and Constraints. New York: Routledge.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 2,
+        title: `Mboho, A. M., Eno-Urua, A. & Ekeanyanwu, N. T., Achibong, F. (2015)`,
+        body: ` The
+        Comfort Years at the University of Uyo (2010-2015). Uyo: University of Uyo`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 3,
+        title: `Obayan, A., Awonuga, C. & Ekeanyanwu, N. T. (2012)`,
+        body: ` The Idea of a University. Ota:
+        Covenant University Press.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 4,
+        title: `Ekeanyanwu, N. T., Ngoa, S. N. and Sobowale, I. (2012)`,
+        body: `  Critique and Application of
+        Communication Theories. Ota: Covenant University Press`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 5,
+        title: `Ekeanyanwu, N. T. & Okeke, C.I.O., [Eds] (2010)`,
+        body: ` Indigenous Societies and Cultural
+        Globalization in the 21st Century: Is the Global Village Truly Real? Germany:
+        VDM Publishing House`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 6,
+        title: `Obe, J., Oyero, S. & Ekeanyanwu, N. [Eds] (2006)`,
+        body: ` Themes in Communication Writing.
+        Lagos: SMC`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 7,
+        title: `Ekeanyanwu, N.T., Aloysius, I. C. & Ogaraku, H.C. (2023)`,
+        body: ` Strategic
+        Communications in 21st Century Nigeria (pp 647-660). In Okunna, C.S. (ed)
+        Communication and Media Studies: Multiple perspectives. Enugu: New
+        Generation Educare.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 8,
+        title: `Mutsvairo, B. and Ekeanyanwu, N. T. (2022)`,
+        body: ` Rethinking Communication Studies in
+        Nigeria: A Methodological and Theoretical Critique. In Mutsvairo, B and Ekeanyanwu, N. T. (Eds) Media and Communication in Nigeria: Conceptual
+        Connections, Crossroads and Constraints. (Pp 1-16). New York: Routledge`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 9,
+        title: `Ekeanyanwu, N. T. & Ajakaiye, Olanrewaju (2020)`,
+        body: ` Political Conflicts and the
+        Imperative of Peace Journalism in Nigeria In Olatunji, R. W.; OgwezzyNdisika, A. O.; Jimoh, J. (Eds). Media, Governance and Sustainable
+        Development in Nigeria (Festschrift in Honour of Professor Lai Oso at 60). (pp
+        121-134). Lagos: LASU Publishers`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 10,
+        title: `Ekeanyanwu, N. T. (2019)`,
+        body: ` Media Effect, No Effect: A Critical Evaluation in Nsereka
+        Barigbon (ed). Critical Media Discourse: Readings in Communication Study
+        Controversies (pp 34-58) Port Harcourt: LSB Media Limited`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 11,
+        title: `Ekeanyanwu, N. T. (2017).`,
+        body: ` Western Media Stereotypes and Portrayal of Africa in
+        Akomolafe, A. C.; Asante, M. K. And Nwoye, A. (Eds). We Will Tell Our Own
+        Story: The Lions of Africa Speak! (pp 183-208). New York: Universal Write
+        Publications LLC`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 12,
+        title: `Olumba, E. E. & Ekeanyanwu, N. T. (2016)`,
+        body: ` Boko Haram and Transnational
+        Terrorism in Africa: Implications for Political Communication and Media
+        Development. In D. Wilson (ed). Mass Media and the Electoral Process in
+        Nigeria, Pp 141-164, Uyo: African Council for Communication Education
+        (ACCE).`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 13,
+        title: `Ekeanyanwu, N. T. (2015)`,
+        body: `Media, Politics of Change, and the End of Jonathan’s
+        Presidency Via the 2015 Presidential Election in Nigeria. In Ike, O. F. And
+        Udeze, S. E. (ed). Emerging Trends in Gender, Health & Political
+        Communication in Africa, Pp 166-202, Enugu: Rhyce Kerex Publishers`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 14,
+        title: `Ekeanyanwu, N. T. (2013)`,
+        body: `International Communication: An Introduction. In Okoro
+        N. (ed). Contemporary Readings in Media and Communication Studies, Pp 35-
+        61, Lagos: St. Benedette Publishers Ltd`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 15,
+        title: `Ozuru, E. and Ekeanyanwu, N. T. (2013)`,
+        body: ` Audience Assessment of the Influence of
+        Social Media Networks on the Global News Flow Controversy. In Wilson, D.
+        (ed) Communication and the New Media in Nigeria (Social Engagements,
+        Political Development and Public Discourse), Pp 85-132, Nigeria: African
+        Council for Communication Education.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 16,
+        title: `Ekeanyanwu, N. T. (2013)`,
+        body: ` Media Freedom, Political Conflicts, and the Nigerian
+        Cultural Dialectics: The American Example. In Umaru, P., Chinenye N., and
+        Nsikak, I. (Eds). Politics, Culture and the Media in Nigeria. Pp.139-155,
+        Ibadan: Stirling-Horden Publishers`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 17,
+        title: `Ekeanyanwu, N. T. & Okorie, N. (2013)`,
+        body: ` Advertising, Semiotics and Strategic Brand
+        Management. In Olatunji, R. & Laninhun (Eds). Dimensions of Advertising
+        Theory and Practice in Africa. (Pp. 36-50). Dakar, Senegal: AMALION
+        PUBLISHING.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 18,
+        title: `Ekeanyanwu, N. T. (2012)`,
+        body: ` Theorizing cultural development vis-à-vis cultural
+        imperialism theory. In N. T. Ekeanyanwu, S. N. Ngoa, I. A. Sobowale (Eds.)
+        Critique and application of communication theories (pp 28-49). Ota: Covenant
+        University Press.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 19,
+        title: `Ekeanyanwu, N. T. (2011)`,
+        body: `Nollywood and Nigeria’s Indigenous Cultural Values:
+        The Developmental Imperatives. In Soola, E. O., et al (Eds) Communication
+        and Africa’s Development Crisis. Pp. 131-143, Germany: VDM Publishing
+        House`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 20,
+        title: `Ekeanyanwu, N. T. (2010)`,
+        body: ` International Public Relations Part1. In Mass Media &
+        Communication: Social and Entrepreneurial Imperatives Vol.3, Pp. 299-318,
+        Ibadan: Stirling-Horden Publishers`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 21,
+        title: `Ekeanyanwu, N. T. (2010)`,
+        body: ` International Public Relations Part11. In Mass Media &
+        Communication: Social and Entrepreneurial Imperatives Vol. 3, Pp. 319-340,
+        Ibadan: Stirling-Horden Publishers.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 22,
+        title: `Ekeanyanwu, N. T. (2010)`,
+        body: ` International/Foreign Correspondence for the Modern
+        Journalist: Satellites and Global Mass Media – Part1. In Mass Media &
+        Communication: Technological Imperatives Vol.4, Pp.1-33, Ibadan: StirlingHorden Publishers.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 23,
+        title: `Ekeanyanwu, N. T. (2010)`,
+        body: ` International/Foreign Correspondence for the Modern
+        Journalist: Economics of International Correspondence – Part11. In Mass
+        Media & Communication: Technological Imperatives Vol.4, Pp.34-44, Ibadan:
+        Stirling-Horden Publishers.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 24,
+        title: `Ekeanyanwu, N. T. (2009)`,
+        body: ` Culture, Communication and Human Development in
+        Nigeria. In Simpson, A. & Akinsola, E. F. (Eds). Culture, Media, Technology
+        and Psycho-Social Development in Nigeria. pp.63-82, Lagos: Interlingua.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 25,
+        title: `Ekeanyanwu, N. T. (2008)`,
+        body: ` News Flow Controversy: The Global Media Coverage of
+        Nigerian Democracy, In Omu, F. I. A. & Oboh, G. E. (Eds.) Mass Media in
+        Nigerian Democracy. Pp.226-261, Benin City: Stirling-Horden Publishers.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 26,
+        title: `Ekeanyanwu, N. T. (2008)`,
+        body: `Africa And Global Challenges: The Need For Integrated
+        Development Communication In Mojaye, E. M., Oyewo, O. O., M’Bayo, R.,
+        and Sobowale, I. A. (eds.) Globalisation And Development Communication In
+        Africa. (Pp 83-99) Ibadan: University of Ibadan Press.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 27,
+        title: `Ekeanyanwu, N. T. (2006)`,
+        body: ` Contemporary Issues in Critical Writing and Reviews In
+        Folarin, A. B., Obe, J.; Oyero, S. & Ekeanyanwu, N. T.; [Eds] (2006). Themes
+        in Communication Writing. Pp.85-111, Lagos: SMC.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 28,
+        title: `Ekeanyanwu, N. T. (2005)`,
+        body: ` The Nigerian Mass Media and Political Conflict
+        Reporting: A Case Study of the Ngige-Uba Political Conflict in Anambra State,
+        in A. M. Yakubu et al (eds) Conflict and Crisis Management In Nigeria Since
+        1980. pp.221-235, Kaduna: Nigeria Defence Academy`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+
+]
+
+const bookMapping = () => {
+    let bookDivEle = document.querySelector('.bookObj');
+    let bookStr = "";
+    book.map((bookDisplay) => {
+        const { title, body, download } = bookDisplay;
+        bookStr +=
+        `
+        <li class="one_third">
+        <article>
+          <h6>${title}</h6>
+            <p class="text-sm"> 
+            ${body} 
+            </p>
+            <p class="downloadbtnbook"><a  href="#"download="" title="Download">${download}</a>
+            </p>
+        </article>
+      </li>
+        `
+    })
+    bookDivEle.innerHTML = bookStr;
+
+}
+bookMapping()
---
+++ layout/scripts/conference.js
@@ -0,0 +1,258 @@
+const conference = [
+    {
+        id: 1,
+        title: `Ekeanyanwu, N.T. (2005)`,
+        body: `Culture and human development in 21st century
+        Africa: the need for cultural homogenization and synchronization. Being a
+        paper presented to the international conference on human development
+        (ICHD) organized by the college of human development, Covenant
+        University, Ota, Ogun state between the 27th-30th June 2005`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 2,
+        title: `Ekeanyanwu, N.T. (2005)`,
+        body: ` The Nigerian mass media and political conflict
+        reporting: a case study of the Ngige-Uba political conflict in Anambra state.
+        Being a paper presented to the conference on conflict and crisis management
+        in Nigeria since 1980. Organized by the Nigeria Defense Academy, Kaduna
+        between the 15th -17th June 2005.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 3,
+        title: `Ekeanyanwu, N.T. (2005)`,
+        body: ` Globalization, information technology and
+        cultural development in a developing world: the place of the communication
+        industry. Being a paper presented to ACCE conference hosted by the
+        Department of Mass Communication, Benue State University, Makurdi`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+    {
+        id: 4,
+        title: `Ekeanyanwu, N.T. (2007)`,
+        body: ` Africa and global images: the need for
+        integrated development communication. Being a paper presented to the
+        Ebenezer Soola conference on communication and development at the Lady
+        Bank Anthony Hall, University of Ibadan, Ibadan Between the 18th – 19th
+        October 2007`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 5,
+        title: `Ekeanyanwu, N.T. & Adekoya, T. (2008)`,
+        body: ` Media handling of political
+        conflicts in a pluralistic society: a study of the Obasanjo-Atiku political
+        conflict. Being a paper presented to ACCE conference hosted by the
+        Department of Mass Communication, Delta State University, Abraka.
+        Between The 6th – 8th August 2008.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 6,
+        title: `Ekeanyanwu, N.T. (2008)`,
+        body: ` Indigenous cultures and ICT in a globalized
+        world: the Nigerian experience. Being a paper accepted for presentation at a
+        South African communication association (SACOMM) conference hosted
+        by the Department of Communication, University of Pretoria, South Africa.
+        Between the 17th-19th of September, 2008`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 7,
+        title: `Ekeanyanwu, N.T. (2009)`,
+        body: ` The Cultural Implications of ICT and
+        Globalization in a in a developing world: The Nigerian Experience. Being a
+        paper presented to ACCE conference hosted by the School of
+        Communication, University of Ghana, Legon, Ghana between August 11-13,
+        2009.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 8,
+        title: `Ekeanyanwu, N.T. (2010)`,
+        body: ` Nollywood, Indigenous Societies and
+        Globalization: The Nigerian Dilemma. Being a paper presented to ACCE
+        conference hosted by the School of Communication, American University of
+        Nigeria, Yola, between October 11-13, 2010`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 9,
+        title: ``,
+        body: `Usaini, Suleimanu and Ekeanyanwu, N.T. (2010). Perceived Role of
+        Entertainment Television in Shaping Social Behaviour of Teenagers. Being a paper presented to ACCE conference hosted by the School of
+        Communication, American University of Nigeria, Yola, between October
+        11-13, 2010.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 10,
+        title: `Ekeanyanwu, N. T. (2011)`,
+        body: ` Nollywood and Nigeria’s Indigenous Cultural
+        Values: The Developmental Imperatives. Being a paper presented to the Prof.
+        Des Wilson Conference on Communication hosted by the Department of
+        Communication Arts, University of Uyo, Akwa Ibom State between March
+        17-18, 2011.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 11,
+        title: `Ekeanyanwu, N. T. (2011)`,
+        body: ` Theorizing Cultural Development Vis-à-vis
+        Cultural Imperialism Theory: Lessons from Nigeria. Being a paper accepted
+        for presentation at the Annual International Convention of AEJMC in St.
+        Louis, Missouri, United States of America, for the Cultural and Critical
+        Studies Division.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 12,
+        title: `Ekeanyanwu, N. T. (2011)`,
+        body: ` Press Coverage of Nigerian President
+        Yar’Adua’s Pre-Election Campaign: A Case Study on Agenda-setting. Being
+        a paper accepted for presentation at the Annual International Convention of
+        AEJMC in St. Louis, Missouri, United States of America, for the Political
+        Communication Interest Group`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 13,
+        title: `Owolabi, T. A., Igbinoba, A. O. & Ekeanyanwu, N. T. (2011)`,
+        body: ` Movie
+        Preference among Nigerian Undergraduates: A Comparative Study of
+        Covenant University and Olabisi Onabanjo University. Being a paper
+        accepted and presented at the Nollywood in Africa, Africa in Nollywood
+        International Conference organized by the School of Media and
+        Communication (SMC), Pan-African University, Lagos from July 21-23,
+        2011.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 14,
+        title: `Ekeanyanwu, N. T. (2011)`,
+        body: ` SUSI 2011: Expectations and Experiences.
+        Being a paper presented at the Institute of International Journalism (IIJ),
+        Scripps College of Communication, Ohio University, Athens, Ohio, United
+        States of America, for the Study of the United States Institutes on Media and
+        Journalism held at Ohio University August 16 2011.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 15,
+        title: `Ekeanyanwu, N. T. & Iruonagbe, C. T. (2012)`,
+        body: ` Role of the Media in
+        Education in Nigeria and the Perception of Nigerian Higher Education by
+        Other Countries. Being a paper presented at a Seminar organized by
+        Fayetteville State University, Fayetteville, North Carolina, USA, from May
+        9-11, 2012.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 16,
+        title: `Nwadike, F. O. and Ekeanyanwu, N. T. (2012)`,
+        body: ` Building Sustainable Peace
+        in Africa: Nigeria in Perspective. Being a paper presented at a Peace
+        Conference organized by ACF Accra, Ghana, from August 9-11, 2012.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 17,
+        title: `Ekeanyanwu, N. T. and Kalyango, Y. (2012).`,
+        body: ` Rethinking International
+        News Flow and Communication in the Era of Social Media Influence. Being a paper presented at the 15th National Conference of the African Council for
+        Communication Education (ACCE) held at the Federal University of
+        Technology, Minna between November 4 – 6, 2012`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 18,
+        title: `Ekeanyanwu, N. T. and Peter, A. S. (2014)`,
+        body: ` Children, Youths and Mediated
+        Violence: A Reflective Evaluation of some Selected Theoretical Models.
+        Being a paper presented at the 16th National Conference of the African
+        Council for Communication Education (ACCE) held at the held at the
+        University of Nigeria, Nsukka, from March 10 – 12, 2014.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 19,
+        title: `Peter, A. S., Sobowale, I. A. & Ekeanyanwu, N. T. (2014)`,
+        body: ` Theory of
+        Planned Behaviour: Measuring Adolescents Media Literacy and Alcohol
+        Drinking Expectances. Being a paper presented at the 16th National
+        Conference of the African Council for Communication Education (ACCE)
+        held at the University of Nigeria, Nsukka, from March 10 – 12, 2014.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 20,
+        title: `Ekeanyanwu, N. T. and Ajakaiye, L. (2015)`,
+        body: ` Nigerian Media, BokoTerrorism, and the Politics of the 2015 Nigerian General Elections. Being a
+        paper presented at the 17th National Conference of the African Council for
+        Communication Education (ACCE) held at the University of Calabar,
+        Calabar, Cross River State from October 20 – 23, 2015`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 21,
+        title: `Ekeanyanwu, N. T. (2016)`,
+        body: ` Economics of Decline and Survival Strategies
+        in the Nigerian Media Industry. Being a paper presented at the 18th National
+        Conference of the African Council for Communication Education (ACCE)
+        held at the University of Port Harcourt, Port Harcourt, Rivers State from
+        October 25 – 28, 2016.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 22,
+        title: `Ekeanyanwu, N. T. (2017)`,
+        body: ` Economics of Decline and Survival Strategies
+        of the American Print Media Industry. Being a paper presented at the 100th
+        International Conference of the Association of Educators of Journalism and
+        Mass Communication (AEJMC) held at the Downtown Marriot Chicago,
+        United States, from August 9 – 12, 2017`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 23,
+        title: `Ekeanyanwu, N. T. (2018)`,
+        body: ` Social Media: Platforms for Information or
+        Disinformation? Being a paper presented as a Lead Speaker at a one-day
+        Workshop on “Hate Communication in Nigeria: Identifying its Roots and
+        Remedies” held at the Barcelona Hotel, Abuja on Thursday, February 22,
+        2018.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 24,
+        title: `Ekeanyanwu, N. T. (2018)`,
+        body: ` Social Media, Hate Communication and
+        National Development in Nigeria. Being a paper presented as a Lead
+        Speaker at the maiden undergraduate students’ conference on Language and
+        the Media in National Development held at the Ebitimi Banigo Auditorium,
+        University of Port Harcourt, Rivers State from August 8-10, 2018.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 25,
+        title: `Olumba, E. E., Ekeanyanwu, N. T. and Uba, C. D. (2018)`,
+        body: ` Migration,
+        Insecurity and Eco-violence in the Nigerian Middle Belt Region: Implications for Inclusiveness. Being a paper presented at the 20th National
+        Conference of the African Council for Communication Education (ACCE)
+        held at the School of Media and Communication, Pan-Atlantic University,
+        Lekki, Lagos State from October 23 – 26, 2018`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },{
+        id: 26,
+        title: `Ekeanyanwu, N. T. (2018)`,
+        body: ` Diasporic Media and Identity Formation
+        Among African Academics Working in Foreign Universities. Being a paper
+        presented at the international conference organized by the Nordic African
+        Institute in Sweden from September 19, 2018 to September 21, 2018.`,
+        download: `<a class="btn inverse" href="#"download="" title="Download">Download</a>`
+    },
+]
+
+const conferenceMapping = () => {
+    let conferenceDivEle = document.querySelector('.conferenceObj');
+    let conferenceStr = "";
+    conference.map((conferenceDisplay) => {
+        const { title, body, download } = conferenceDisplay;
+        conferenceStr +=
+        `
+        <li class="one_third">
+        <article>
+          <h6>${title}</h6>
+            <p class="text-sm"> 
+            ${body} 
+            </p>
+            <p class="downloadbtn"><a  href="#"download="" title="Download">${download}</a>
+            </p>
+        </article>
+      </li>
+        `
+    })
+    conferenceDivEle.innerHTML = conferenceStr;
+
+}
+conferenceMapping()
